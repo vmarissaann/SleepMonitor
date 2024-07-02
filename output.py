069556a340d3307fa556daf6090708c0a135bcc1
richjpex <richard_pecson@dlsu.edu.ph>
2024-07-02 18:25:08 +0800

    modified: added avg times

--- output.py
+++ output.py
@@ -7,12 +7,16 @@ serport = serial.Serial('/dev/cu.usbserial-57670014761', 115200)
 
 total_movement_time = 0
 total_idle_time = 0
+avg_movement_time = 0
+avg_idle_time = 0
+movement_count = 0
+idle_count = 0
 previous_time = None
 previous_state = None
 
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Timestamp", "State", "Duration", "Total Idle Time", "Total Movement Time"])
+    writer.writerow(["Timestamp", "State", "Duration", "Total Idle Time", "Total Movement Time", "Average Idle Time", "Average Movement Time"])
 
 while True:
     current_time = datetime.datetime.now()
@@ -20,8 +24,10 @@ while True:
 
     if "Movement detected!" in buffer:
         current_state = "Movement"
+        movement_count += 1
     elif "No movement." in buffer:
         current_state = "Idle"
+        idle_count += 1
     else:
         current_state = "Unknown"
 
@@ -29,12 +35,14 @@ while True:
         duration = round((current_time - previous_time).total_seconds(), 2)
         if previous_state == "Movement":
             total_movement_time += duration
+            avg_movement_time = total_movement_time / movement_count
         elif previous_state == "Idle":
+            avg_idle_time = total_idle_time / idle_count
             total_idle_time += duration
 
     with open('output.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([current_time, current_state, duration if previous_state else 0, total_movement_time, total_idle_time])
+        writer.writerow([current_time, current_state, duration if previous_state else 0, total_movement_time, total_idle_time, avg_movement_time, avg_idle_time])
 
     previous_time = current_time
     previous_state = current_state
